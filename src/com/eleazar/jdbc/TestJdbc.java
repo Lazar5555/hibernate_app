bf44017ca665e9dd6fca416c676481db78664812
Eleazar Ponce <poncebe@gmail.com>
2018-04-12 18:54:18 -0500

    Initial commit.

---
+++ src/com/eleazar/jdbc/TestJdbc.java
@@ -0,0 +1,24 @@
+package com.eleazar.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+
+	public static void main(String[] args) {
+		String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false";
+		String user = "hbstudent";
+		String pass = "hbstudent";
+		
+		try {
+			System.out.println("Connecting to: " + jdbcUrl);
+			Connection myConn = DriverManager.getConnection(jdbcUrl, user, pass);
+			
+			System.out.println("Connection succesful!");
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+
+	}
+
+}
